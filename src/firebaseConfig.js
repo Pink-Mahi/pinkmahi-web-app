8a0d38c7692e667e6fb6e2c04f7f84f1ade399fb
Pink-Mahi <pinkmahitech@gmail.com>
2024-09-13 14:23:24 -0400

    update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,17 +3,6 @@ import { getDatabase } from 'firebase/database'; // Import Realtime Database
 import { getFirestore } from 'firebase/firestore'; // Import Firestore
 import { getAuth } from 'firebase/auth'; // Import Firebase Auth
 
-// Firebase configuration
-//const firebaseConfig = {
-//apiKey: 'AIzaSyDobV7xUQ6JemlZUKQcdtLWfFGOzUMUn2w',
-// authDomain: 'esp32sensordata-c601b.firebaseapp.com',
-//  databaseURL: 'https://esp32sensordata-c601b-default-rtdb.firebaseio.com',
-// projectId: 'esp32sensordata-c601b',
-// storageBucket: 'esp32sensordata-c601b.appspot.com',
-// messagingSenderId: '413357863110',
-// appId: '1:413357863110:web:9f117b6fce74247bac67c7',
-//};
-
 const firebaseConfig = {
   apiKey: 'AIzaSyCkzLCQeumFcc8LioAsFNqHltgDQSFcgHg',
 
